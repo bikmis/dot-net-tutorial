d565a89214003ff0115d52595a2228b673780b6e
Bikash Mishra <bikashmishra.developer@gmail.com>
2020-12-27 16:46:21 -0500

    removed steps from the class

--- MyWebApplicationForAsmxWebService/MathService.asmx.cs
+++ MyWebApplicationForAsmxWebService/MathService.asmx.cs
@@ -7,14 +7,7 @@ using System.Web.Services;
 namespace MyWebApplicationForAsmxWebService
 {
     /// <summary>
-    /// How to creat a web service (asmx service): 
-    /// 1. Add a project (ASP.NET Web Application (.NET Framework) to a solution
-    /// 2. Rigt-click on the project and add a new item (Web Service(ASMX))
-    /// 3. Rename the service to MathService.asmx and add the following web methods: Add, Subtract etc.
-    /// 4. Right-click on the MathService.asmx and click on Open With... and select Web Service Editor (Default) to open MathService.asmx
-    ///    to view the CodeBehind and Class name: 
-    ///    <%@ WebService Language="C#" CodeBehind="MathService.asmx.cs" Class="MyWebApplicationForAsmxWebService.MathService" %>
-    /// 5. You run the site by clicking F5, and type in https://localhost:44384/MathService.asmx in the browser to test web methods.
+    /// MyWebApplicationForAsmxWebService.MathService
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
