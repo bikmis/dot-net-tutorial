88d3b626914ebc993096d3530dc9d302114a9c72
Bikash Mishra <bikashmishra.developer@gmail.com>
2020-12-31 18:52:16 -0500

    writer service

---
+++ MyWcfServiceLibrary/IWriterService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWcfServiceLibrary
+{
+    [ServiceContract]
+    public interface IWriterService
+    {
+        [OperationContract]
+        string GetWriter(string name);
+
+        [OperationContract]
+        List<Book> GetBooks(string writer);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "MyWcfServiceLibrary.ContractType".
+    [DataContract]
+    public class Book
+    {
+        public string Title { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ MyWcfServiceLibrary/WriterService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class WriterService : IWriterService
+    {
+        public string GetWriter(string name)
+        {
+            return name;
+        }
+
+        public List<Book> GetBooks(string writer)
+        {
+            if (writer == "Shakespeare")
+            {
+                List<Book> books = new List<Book>() { new Book() { Title = "Macbeth", Price = 2.35 } };
+                return books;
+            }
+            else
+                return null;
+        }
+    }
+}
