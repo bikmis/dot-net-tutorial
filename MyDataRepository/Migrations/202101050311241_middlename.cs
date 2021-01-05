c34940a0358f159b667024d302a94b6f4c47577d
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-01-04 22:13:27 -0500

    migrations

---
+++ MyDataRepository/Migrations/202101050311241_middlename.cs
@@ -0,0 +1,18 @@
+﻿namespace MyDataRepository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class middlename : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Students", "MiddleName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Students", "MiddleName");
+        }
+    }
+}
