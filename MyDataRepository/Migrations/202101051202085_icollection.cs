76857de93395c90cd6f2e828dfea9f99e9c9d241
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-01-05 07:03:56 -0500

    changed to icollection from list

---
+++ MyDataRepository/Migrations/202101051202085_icollection.cs
@@ -0,0 +1,16 @@
+﻿namespace MyDataRepository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class icollection : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
