6486b8cd66f1e42cb82264b4b912ec87b74fc2c7
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-01-05 07:43:46 -0500

    many to many relationship with icollection in both subject and course tables

--- MyDataRepository/Entities/Subject.cs
+++ MyDataRepository/Entities/Subject.cs
@@ -8,6 +8,6 @@ namespace MyDataRepository.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-      //  public ICollection<Course> Courses { get; set; }
+        public ICollection<Course> Courses { get; set; }
     }
 }
