7e14d626d0c475d49eb187399918cdd619356db6
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-01-21 17:40:52 -0500

    comment on school context

--- MyDataRepository/SchoolContext.cs
+++ MyDataRepository/SchoolContext.cs
@@ -9,6 +9,15 @@ namespace MyDataRepository
 {
     internal class SchoolContext : DbContext
     {
+        /* This library is built using .net framework and so .net core mvc cannot use it.
+           add a reference to System.Configuration to use ConfigurationManager,
+           SchoolDb should be a ConfigurationString in web.config of the calling project as follows:
+           <connectionStrings>
+                <add name="SchoolDb"
+                     connectionString="Data Source=localhost; Initial Catalog=SchoolDb; Integrated Security=True;"
+                     providerName="System.Data.SqlClient" />
+           </connectionStrings>
+         */
         static string connectionString = ConfigurationManager.ConnectionStrings["SchoolDb"].ToString(); //"Data Source=localhost;Initial Catalog=SchoolDb;Integrated Security=true;";
 
         //An instance of the context class represents "Unit of Work" and Repository Pattern wherein it can combine multiple changes under a single database transaction.
