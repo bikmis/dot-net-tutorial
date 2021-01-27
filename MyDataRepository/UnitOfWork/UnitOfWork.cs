ff33f0f34d69dbf0cb9a9a1340b0d22b470ab6ec
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-01-26 21:44:48 -0500

    register student

--- MyDataRepository/UnitOfWork/UnitOfWork.cs
+++ MyDataRepository/UnitOfWork/UnitOfWork.cs
@@ -79,6 +79,9 @@ namespace MyDataRepository.UnitOfWork
         public Student RegisterStudent(int id)
         {
             var student = StudentRepository.GetById(id);
+            student.Gender = "Female";
+            studentRepository.Update(student);
+            Save();
             // student is registered.
             return student;
         }
