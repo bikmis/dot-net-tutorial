c24584fab883f33cf2863165f3ccbb6e7e9d663d
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-01-30 12:57:59 -0500

    web service recreated

---
+++ MyWCFService/App_Code/NameService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+
+
+public class NameService : INameService
+{
+	public string GetName(string name)
+	{
+		return string.Format("You entered: {0}", name);
+	}
+
+	public FullName GetFullName(string firstName, string lastName)
+	{
+		FullName fullName = new FullName() { FirstName = firstName, LastName = lastName };
+		return fullName;
+	}
+}
---
+++ MyWCFService/App_Code/TestService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class TestService : ITestService
+{
+
+    public string SaySomething(string something)
+    {
+        return string.Format("You said: {0}", something);
+    }
+}
