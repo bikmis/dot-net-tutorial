12dbc96682ed927a45a77c8b40d22e529089c18f
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-01-30 12:28:43 -0500

    console app, remove wcf service

---
+++ MyConsoleApp/Func_T_TResult_Delegate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyConsoleApp
+{
+    public class Func_T_TResult__Delegate
+    {
+        static Func<string, string> selector = str => str.ToUpper();
+        static List<string> words =new List<string>() { "orange","apple", "Article", "elephant" };
+
+        public static void ConvertToUppercase() {
+            var wordsInUppercase = words.Select(selector).ToList();
+
+            wordsInUppercase.ForEach(word => {
+                Console.WriteLine(word);
+            });
+        }
+    }
+}
