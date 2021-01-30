2c7e4d88a509b816fe5f755a46d9e5d6aa32e3d2
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-01-30 16:48:39 -0500

    comment added

--- MyConsoleApp/Func_T_TResult_Delegate.cs
+++ MyConsoleApp/Func_T_TResult_Delegate.cs
@@ -6,11 +6,11 @@ namespace MyConsoleApp
 {
     public class Func_T_TResult__Delegate
     {
-        static Func<string, string> selector = str => str.ToUpper();
+        static Func<string, string> selector = str => str.ToUpper(); // A lambda expression is assigned to a Func variable (delegate)
         static List<string> words =new List<string>() { "orange","apple", "Article", "elephant" };
 
         public static void ConvertToUppercase() {
-            var wordsInUppercase = words.Select(selector).ToList();
+            var wordsInUppercase = words.Select(selector).ToList();  // Select takes a delegate
 
             wordsInUppercase.ForEach(word => {
                 Console.WriteLine(word);
