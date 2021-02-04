47c6e6405fe708a4130431d5afdeadb271864ae5
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-02-03 20:35:30 -0500

    nullable datetime

--- MyDataWithEF6/Entities/BaseEntity.cs
+++ MyDataWithEF6/Entities/BaseEntity.cs
@@ -9,11 +9,11 @@ namespace MyDataWithEF6.Entities
     public class BaseEntity
     {
         public int Id { get; set; }
-        public string CreatedDatetime { get; set; }
+        public DateTime CreatedDatetime { get; set; }
         public string CreatedBy { get; set; }
-        public DateTime UpdatedDatetime { get; set; }
+        public DateTime? UpdatedDatetime { get; set; }
         public string UpdatedBy { get; set; }
-        public DateTime DeletedDatetime { get; set; }
+        public DateTime? DeletedDatetime { get; set; }
         public string DeletedBy { get; set; }
     }
 }
