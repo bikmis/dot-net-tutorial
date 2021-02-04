970a1904683747fba97dfd85a61a63c63a0ddfe4
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-02-03 20:00:08 -0500

    BaseEntity class added

---
+++ MyDataWithEF6/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataWithEF6.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string CreatedDatetime { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedDatetime { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime DeletedDatetime { get; set; }
+        public string DeletedBy { get; set; }
+    }
+}
