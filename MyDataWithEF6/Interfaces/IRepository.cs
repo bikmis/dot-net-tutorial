4ec5de9bb215064010cdf4f90651bd0f921d454f
Bikash Mishra <bikashmishra.developer@gmail.com>
2021-02-13 14:47:20 -0500

    Expression added to interface

--- MyDataWithEF6/Interfaces/IRepository.cs
+++ MyDataWithEF6/Interfaces/IRepository.cs
@@ -10,6 +10,8 @@ namespace MyDataWithEF6.Interfaces
     {
         IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
         TEntity GetById(int id);
+        // TEntity Get(Expression<Func<TEntity, bool>> expression = null, List<string> includes = null);
+        // bool IsExists(Expression<Func<TEntity, bool>> expression)
         IEnumerable<TEntity> GetAll();
         void Insert(TEntity entity);
         void Update(TEntity entity);
